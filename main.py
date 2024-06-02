64f6309939713449be5ccd020275e11c92ad4fe8
Pat <patrick@satyshur.net>
2024-06-02 11:41:37 -0500

    Add generic main.py file.

---
+++ main.py
@@ -0,0 +1,10 @@
+'''main.py
+
+User code should start in here. This file takes the place of code.py from the default CPy behavior.
+'''
+
+import time
+
+while True:
+    print("Hello World!")
+    time.sleep(1)
