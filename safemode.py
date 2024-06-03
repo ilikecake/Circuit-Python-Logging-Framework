6b48f3d413d864f19e002ddb8b9fb8448c112e33
Pat <patrick@satyshur.net>
2024-06-02 23:13:41 -0500

    Catch safe mode entry.
    Catch safe mode entry and log it. Restart after logging if LOG_AUTO_RESTART is set.

--- safemode.py
+++ safemode.py
@@ -9,19 +9,25 @@ hardware error with the flash memory.
 
 '''
 import microcontroller
-import supervisor
 import storage
 import time
 import adafruit_logging as logging
+import supervisor
+import os
+import traceback
 
-fs_obj = storage.getmount("/")
-if fs_obj.readonly is False:
-    #the file system is writable by CircuitPython.
-    logger = logging.getLogger('error_log')
-    logger.addHandler(logging.RotatingFileHandler('log.txt', 'a', 1000, 3))     #TODO: Control these in settings.toml. also make auto-reboot an option in there
+try:
+    storage.remount("/", readonly=False)
+    logger = logging.getLogger(os.getenv("LOG_NAME"))
+    logger.addHandler(logging.RotatingFileHandler(os.getenv("LOG_FILENAME"), 'a', os.getenv("LOG_FILE_MAX_SIZE"), os.getenv("LOG_FILES_TO_KEEP")))
     logger.setLevel(logging.ERROR)
+    logger.critical(f"Controller entered safemode: {supervisor.runtime.safe_mode_reason}")
 
-    logger.critical(supervisor.runtime.safe_mode_reason)
-    
-    time.sleep(20)
-    microcontroller.reset()
+    if os.getenv("LOG_AUTO_RESTART") is "True":
+        time.sleep(20)
+        microcontroller.reset()
+        
+except Exception as e:
+    #If there is some issue writing the safemode reason to the log file, try to write *that* error to the log file.
+    #This was useful for debugging, but probably won't do anything in real code.
+    logger.error(traceback.format_exception(e))
